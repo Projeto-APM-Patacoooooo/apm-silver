1283afdaae36c8eb35d525b95324772aed8189db
Eduardo Bezerra <eduardo.escolar447@gmail.com>
2025-02-14 09:25:57 -0300

    Servidor web e página inicial criados com sucesso!

---
+++ server/main.js
@@ -0,0 +1,29 @@
+/*
+    Este código é o coração do nosso servidor, 
+    então, por favor, rapazes, não saiam mexendo nele sem saber o que estão fazendo.
+
+    Deixei tudo bem comentado caso queiram mudar alguma funcionalidade.
+
+    E lembrem-se: não subam nada no GitHub antes de testar suas alterações!
+
+    Seu amigo: Eduardo Bezerra
+*/
+
+/* 
+    Abaixo estamos simplesmente importanto a biblioteca express.
+    Ela é uma biblioteca que o APM Silver utiliza para manter e 
+    gerenciar o servidor web.
+*/
+const express = require('express');
+
+const path = require('path'); //Biblioteca nativa do node para facilitar a navegação por caminhos de diretórios
+
+const servidor = express(); // Aqui nós estamos criando a instância do nosso servidor web
+
+servidor.use(express.static(path.join(__dirname, 'public'))); //Estamos dizendo ao express que todas os assets e páginas HTML estão dentro da pasta "public"
+
+let porta = 8080; //A porta em que o servidor vai rodar localmente (precisamos conversar com o professor Ivan sobre como configurar o nosso servidor para rodar num domínio)
+
+servidor.listen(porta, () => {
+    console.log(`[Servidor]: Ei, eu estou rodando na porta ${porta}!`);
+}); //E finalmente colocamos o nosso servidor web para rodar
