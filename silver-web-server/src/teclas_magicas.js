80e6053d9cd8f1e8404578e8b5b2d12d045eb127
Pardoardo <eduardo.escolar447@gmail.com>
2025-06-09 21:53:11 -0300

    leitura de performance

--- silver-web-server/src/teclas_magicas.js
+++ silver-web-server/src/teclas_magicas.js
@@ -20,10 +20,27 @@ function TeclasMagicas(callBackDeManutencao) {
                     manutencao = false;
                 };
                 callBackDeManutencao(manutencao);
-            break;
+                break;
+            case 'p':
+                const memoryUsage = process.memoryUsage();
+
+                // Convertendo para MB
+                const rssMB = memoryUsage.rss / (1024 * 1024);
+                const heapTotalMB = memoryUsage.heapTotal / (1024 * 1024);
+                const heapUsedMB = memoryUsage.heapUsed / (1024 * 1024);
+                const externalMB = memoryUsage.external / (1024 * 1024);
+                
+                console.log("========================================================")
+                console.log("Informações de Performance:")
+                console.log("========================================================")
+                console.log(`RSS: ${rssMB.toFixed(2)} MB`);
+                console.log(`Heap Total: ${heapTotalMB.toFixed(2)} MB`);
+                console.log(`Heap Used: ${heapUsedMB.toFixed(2)} MB`);
+                console.log(`External: ${externalMB.toFixed(2)} MB`);
+                break;
             case '\u0003':
                 process.exit();
-            break;
+                break;
         }
     })
 }
