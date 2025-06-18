f8de806fbe08efc3be336bd458e4639ec25c8ca8
Pardoardo <eduardo.escolar447@gmail.com>
2025-06-17 21:48:36 -0300

    Primeiros passos com CRUD de metas

--- silver-web-server/public/assets/js/car_dash_metas.js
+++ silver-web-server/public/assets/js/car_dash_metas.js
@@ -20,10 +20,19 @@ xhr.onreadystatechange = () => {
                 var titulo = alvoAtual.titulo_meta;
                 var batida = alvoAtual.batida;
 
+                switch(alvoAtual.batida){
+                    case 1: 
+                        batida = "Sim"
+                    break;
+                    case 0: 
+                        batida = "Não"
+                    break;
+                }
+
                 var data = new Date(data_pub); 
                 var formatada = new Intl.DateTimeFormat('pt-BR').format(data);
 
-                alvoInstituicoes.innerHTML += `<tr><td scope="row">${titulo}</td><td>${data}</td><td>${batida}</td><td><button class="btn btn-primary" onclick=irParaPaginaDeEdicao(${alvoAtual.id_noticia})>Editar</button></td><td><button class="btn btn-danger" onclick=IniciarExclusao(${alvoAtual.id_noticia})>Excluir</button></td><tr>`
+                alvoInstituicoes.innerHTML += `<tr><td scope="row">${titulo}</td><td>${formatada}</td><td>${batida}</td><td><button class="btn btn-primary" onclick=irParaPaginaDeEdicao(${alvoAtual.id_noticia})>Editar</button></td><td><button class="btn btn-danger" onclick=IniciarExclusao(${alvoAtual.id_meta})>Excluir</button></td><tr>`
 
 
             }
--- silver-web-server/public/assets/js/carregador_de_metas.js
+++ silver-web-server/public/assets/js/carregador_de_metas.js
@@ -1,52 +1,7 @@
-function BuscarDestaque(id) {
-    return new Promise((resolve, reject) => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                resolve(JSON.parse(this.responseText));
-            } else if (this.readyState == 4) {
-                reject('Erro ao buscar dados');
-            }
-        };
-        xhttp.open("GET", `/meta?id=${id}`, true);
-        xhttp.send();
-    });
-}
-
-function Buscarmeta(id) {
-    return new Promise((resolve, reject) => {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                resolve(JSON.parse(this.responseText));
-            } else if (this.readyState == 4) {
-                reject('Erro ao buscar dados');
-            }
-        };
-        xhttp.open("GET", `/meta?id=${id}`, true);
-        xhttp.send();
-    });
-}
-
-// FASE[2]: BUSCANDO AS NOTÍCIAS EM DESTAQUE EM SI
-
-Promise.all([
-    BuscarDestaque(1),
-    BuscarDestaque(2),
-    BuscarDestaque(3)
-]).then(results => {
-    const dest1 = results[0].id_meta;
-    const dest2 = results[1].id_meta;
-    const dest3 = results[2].id_meta;
-
-    return Promise.all([
-        Buscarmeta(dest1),
-        Buscarmeta(dest2),
-        Buscarmeta(dest3)
-    ]);
-}).then(newsResults => {
-
-    const lbl_meta1 = document.getElementById("lbl_meta1");
+fetch('/metas_recentes')
+    .then(res => res.json())
+    .then(newsResults => {
+        const lbl_meta1 = document.getElementById("lbl_meta1");
     lbl_meta1.textContent = newsResults[0].titulo_meta;
 
     const sts_met1 = document.getElementById("sts_met1");
@@ -96,6 +51,7 @@ Promise.all([
             sts_met3.style.color = "#F0552E";
         break;
     }
-}).catch(error => {
-    console.error(error);
-});
+    })
+    .catch(err => {
+        console.error("Erro ao carregar notícias:", err);
+    });
