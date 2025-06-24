e529aca9d2f15859bebc158504b23dfbe6b5fc92
Pardoardo <eduardo.escolar447@gmail.com>
2025-06-24 20:30:55 -0300

    Colocando caixa  de login na nav bar de todas as páginas.

--- silver-web-server/src/rotas/politica_de_privacidade.js
+++ silver-web-server/src/rotas/politica_de_privacidade.js
@@ -1,7 +1,11 @@
 function rotear(servidor, callbackVerificarMan) {
     servidor.get('/politica-de-privacidade', function (req, res) {
         callbackVerificarMan(res);
-        res.render('pages/politica-de-privacidade')
+        if(req.session.user){
+            res.render('pages/politica-de-privacidade', {logado: true, emailLogado: req.session.user.email})
+        } else {
+            res.render('pages/politica-de-privacidade',  {logado: false});
+        }
     });
 }
 
--- silver-web-server/src/rotas/relatorios.js
+++ silver-web-server/src/rotas/relatorios.js
@@ -1,7 +1,11 @@
 function rotear(servidor, callbackVerificarMan, callbackIsAuth) {
     servidor.get('/relatorios', function (req, res) {
         callbackVerificarMan(res);
-        res.render('pages/relatorios');
+        if(req.session.user){
+          res.render('pages/relatorios', {logado: true, emailLogado: req.session.user.email})
+        } else {
+          res.render('pages/relatorios',  {logado: false});
+        }
     });
     servidor.get('/gerar-relatorios', callbackIsAuth, function (req, res) {
         verificarManutencao(res);
